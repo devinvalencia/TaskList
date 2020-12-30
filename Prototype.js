1ea146dc7c7f8f18f67f9ae969ae86a248b741a3
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2020-12-30 09:24:51 -0500

    updated notes

---
+++ Prototype.js
@@ -0,0 +1,26 @@
+//Object.prototype - All obj inherit properties and method from protos, object literals inheritting from this object
+
+// Object.prototype > yourObj.prototype > instance of obj, any obj can access funcs and props from both
+
+// Prototypes should contain methods and properties that every instance of the object will use or can use
+
+// Whereas objects should contain values that might be unique to that instance
+
+// Java Class = Prototype + Constructor definations since JS doesnt have classes
+
+function Person(firstName, lastName, dob) {
+  this.fistName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+}
+
+Person.prototype.getAge = function() {
+  var diff = Date.now() - this.birthday.getTime();
+  var ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear()-1970);
+}
+
+var obj1 = new Person('Devin','Valencia','10/03/1990');
+console.log(obj1);
+
+
---
+++ constructors.js
@@ -0,0 +1,19 @@
+const human = {
+  name: 'Devin',
+  age: 30
+} // Object Literal, not an object
+
+// Constructor
+function Person(x, dob) {
+  this.name = x;
+  this.birthday = new Date(dob);
+  this.getAge = function() {
+    var diff = Date.now() - this.birthday.getTime();
+    var ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear()-1970);
+  }
+} // Says THIS object property called name is brad
+
+var obj1 = new Person('Devin', '10/03/1990'); // Actual object
+console.log(obj1);
+
